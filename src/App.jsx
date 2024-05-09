470ed9567758e4d18758f7d7f9e67f46f3ff6f5e
Teresa-333 <mconcepcion@intellisys.com.do>
2024-05-09 16:08:42 -0400

    eslint for jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+// import React from "react";
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  
+  )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+export default App;
 
 // const Pet = (props) => {
 //   return React.createElement("div", {}, [
@@ -9,24 +27,19 @@ import Pet from "./Pet";
 //     React.createElement("h2", {}, props.breed),
 //   ]);
 // };
-
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
-};
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(React.createElement(App));
+// const App = () => {
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockatiel",
+  //   }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  // ]);
+// }
